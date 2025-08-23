23b66bda4532b710f2aee52982d9fafc9d5a5cea
Ahmed Kamal <eng.ahmedkamal357@gmail.com>
2025-08-23 10:03:18 +0300

    upgrade route

--- modules/video/routes/api.php
+++ modules/video/routes/api.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Video\App\Http\Controllers\VideoController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('videos', VideoController::class)->names('video');
-});
+// Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+//     Route::apiResource('videos', VideoController::class)->names('video');
+// });
 Route::prefix('videos')->group(function () {
     Route::get('/', [VideoController::class, 'index'])->name('video.index');
     Route::post('/', [VideoController::class, 'create'])->name('video.create');
